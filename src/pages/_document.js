5df11402b7db24dd66a016b97d9bbce843509d12
Prathyaksh Malligari <70006838+theprathyaksh@users.noreply.github.com>
2024-04-06 10:41:11 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.min.js"></script>
-
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          {/* Load PDF.js script asynchronously */}
+          <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.min.js"
+            defer
+          />
+        </body>
+      </Html>
+    );
+  }
 }
